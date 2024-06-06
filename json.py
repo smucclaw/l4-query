27fb0309c69d21172188b05397de0efd452b920d
ymh <hanyongming@gmail.com>
2024-06-06 14:29:56 +0800

    add jsonschema related classes / infra deps (add jsonref)

---
+++ json.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+# from pydantic import BaseModel
+from typing import NewType, Union
+import ujson as json
+from pathlib import Path
+
+JsonSchemaString = NewType("JsonSchemaString", str)
+
+
+@dataclass
+class JsonSchema:
+    _schema_dict: dict
+    _schema_str: JsonSchemaString
+
+    def __init__(self, schema: Union[str, dict]):
+        # TODO: chk if this is a valid json schema
+        # maybe use the approach from `outlines`: https://github.com/outlines-dev/outlines/blob/ed44a47d43ea21446812d891e77ef1e560b61868/outlines/generate/json.py#L46
+
+        if isinstance(schema, str):
+            self._schema_str = JsonSchemaString(schema)
+            self._schema_dict = json.loads(schema)
+        elif isinstance(schema, dict):
+            self._schema_dict = schema
+            self._schema_str = JsonSchemaString(json.dumps(schema))
+
+    def as_schema_string(self) -> JsonSchemaString:
+        return self._schema_str
+
+
+def load_json_schema_dict(schema_path: Path) -> dict:
+    with open(schema_path, "r") as schema_file:
+        schema = json.load(schema_file)
+
+    return schema
